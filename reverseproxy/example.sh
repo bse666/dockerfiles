bdbcb755b510b92dc05280d3598d0158db5927a1
BSE <bse666@gmail.com>
2016-06-08 23:28:43 +0200

    dont have to forward ports for access to client container

--- reverseproxy/example.sh
+++ reverseproxy/example.sh
@@ -1,4 +1,4 @@
 # two hello world containers on port 80
 # and the proxy does the round robin
-docker run -d -e VIRTUAL_HOST=hello-world -p 80 tutum/hello-world
-docker run -d -e VIRTUAL_HOST=hello-world -p 80 tutum/hello-world
+docker run -d -e VIRTUAL_HOST=hello-world tutum/hello-world
+docker run -d -e VIRTUAL_HOST=hello-world tutum/hello-world
