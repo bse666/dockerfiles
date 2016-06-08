0e78adbfb30c0acdb25109e6f35c977841edab1d
BSE <bse666@gmail.com>
2016-06-08 23:39:39 +0200

    ignored temp folders and added cert folder creation

--- reverseproxy/run.sh
+++ reverseproxy/run.sh
@@ -1,4 +1,4 @@
 echo Starting Proxy on 80 and 443
-docker run -d -p 80:80 -p 443:443 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run -d -p 80:80 -p 443:443 -v `pwd`/certs:/etc/nginx/certs -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
 echo "Proxy ready..."
 echo "Run your container with -e VIRTUAL_HOST=domain.local ."
